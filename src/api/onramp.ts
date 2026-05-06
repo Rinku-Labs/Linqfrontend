a16c2796168ff8dfc0e13b0558bad1dbb505174b
danxcrusher <danpromise1960@gmail.com>
2026-05-06 18:54:42 +0100

    Onramp Account name display

--- src/api/onramp.ts
+++ src/api/onramp.ts
@@ -23,7 +23,6 @@ export interface OnrampOrderResponse {
     amountStableCoin: number;
     expiresAt: string;
     status: string;
-    accountName?: string;
 }
 
 export interface OnrampStatusResponse {
